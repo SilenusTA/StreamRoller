4bf6125f5b2e608eca55f75f3b70018eb1d0a6ae
Silenus <silenusta@gmail.com>
2023-06-03 17:45:02 +0100

    Added License header

--- extensions/~~demoextension/demoextension.js
+++ extensions/~~demoextension/demoextension.js
@@ -1,4 +1,25 @@
-// ############################# DEMOEXTENSION.js ##############################
+/**
+ *      StreamRoller Copyright 2023 "SilenusTA https://www.twitch.tv/olddepressedgamer"
+ * 
+ *      StreamRoller is an all in one streaming solution designed to give a single
+ *      'second monitor' control page and allow easy integration for configuring
+ *      content (ie. tweets linked to chat, overlays triggered by messages, hue lights
+ *      controlled by donations etc)
+ * 
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Affero General Public License as published
+ *      by the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ * 
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Affero General Public License for more details.
+ * 
+ *      You should have received a copy of the GNU Affero General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ // ############################# DEMOEXTENSION.js ##############################
 // This is a demo extension you can copy to start a new extension. It is
 // designed to make creating a new extension easier by habing all the templates 
 // you might need.
--- extensions/~~testoverlay/alertscript.js
+++ extensions/~~testoverlay/alertscript.js
@@ -1,4 +1,25 @@
-// ############################## FOLLOWER.js ##################################
+/**
+ *      StreamRoller Copyright 2023 "SilenusTA https://www.twitch.tv/olddepressedgamer"
+ * 
+ *      StreamRoller is an all in one streaming solution designed to give a single
+ *      'second monitor' control page and allow easy integration for configuring
+ *      content (ie. tweets linked to chat, overlays triggered by messages, hue lights
+ *      controlled by donations etc)
+ * 
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Affero General Public License as published
+ *      by the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ * 
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Affero General Public License for more details.
+ * 
+ *      You should have received a copy of the GNU Affero General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ // ############################## FOLLOWER.js ##################################
 // Main overlay script file. This is a demo file to show how to link the backend
 // into your overlay and trigger alters, onscreen data etc
 // -------------------------- Creation ----------------------------------------
